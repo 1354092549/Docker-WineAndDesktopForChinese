b2f97f1ff7239174837c609e1e12bd1c92322dcb
1354092549 <1354092549@qq.com>
2017-04-13 15:25:51 +0800

    优化

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ ! -z $VNC_PASSWORD ];then
+	mkdir -p ~/.vnc
+	x11vnc -storepasswd $VNC_PASSWORD ~/.vnc/passwd
+	unset VNC_PASSWORD
+fi
+export WINEPREFIX='/data/wine'
 export DISPLAY=':1'
 Xvfb $DISPLAY -screen 0 1024x768x16 &
 for i in $(seq 1 10); do
@@ -10,12 +16,9 @@ for i in $(seq 1 10); do
 	sleep 0.5
 done
 jwm &
-if [ -z $VNC_PASSWORD ];then  
+if [ ! -f ~/.vnc/passwd ];then  
 	x11vnc -display $DISPLAY -xkb -forever -shared -nopw &
 else
-	mkdir -p ~/.vnc
-	x11vnc -storepasswd $VNC_PASSWORD ~/.vnc/passwd
-	unset VNC_PASSWORD
 	x11vnc -display $DISPLAY -usepw -xkb -forever -shared -nopw &
 fi
 node /usr/lib/noVNC/websockify/websockify.js --web /usr/lib/noVNC/web 80 localhost:5900
